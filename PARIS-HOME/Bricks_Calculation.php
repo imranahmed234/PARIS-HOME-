96ed77e2605e1eeba312fd45d164c4b429373ae2
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-11-14 20:15:01 +0600

    Add files via upload

---
+++ PARIS-HOME/Bricks_Calculation.php
@@ -0,0 +1,21 @@
+<?php
+
+echo"PARIS-HOME<br>";
+echo"Today Calculation<br>";
+echo"Number of BRICK<br>";
+echo"BRICK-6000<br>";
+echo"1k=13,400<br>";
+$N=6;
+$T=13400;
+
+echo "Total Cost:  $N*$T = ";
+echo $N*$T;
+echo"tk";
+
+
+
+
+
+
+
+?>
