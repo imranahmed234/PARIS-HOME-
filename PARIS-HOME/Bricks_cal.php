545e86703d896145819aa3bfff710accace52f13
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-11-14 21:26:34 +0600

    Add files via upload

---
+++ PARIS-HOME/Bricks_cal.php
@@ -0,0 +1,23 @@
+<html>
+
+    <head>
+        <title>
+            <txt> PARIS-HOME </txt>
+        </title>
+    </head>
+         <body>
+             <h1 align='center'style="background-color:Purple;"> PARIS-HOME </h1>
+             <button style="background-color:Brown"> Bricks</button>
+             <button style="background-color:White"> 6000Tk</button></br>
+             <button style="background-color:Brown"> Bricks</button>
+             <button style="background-color:White"> 1k</button>
+             <button style="background-color:White"> 13400Tk</button><br>
+             <button style="background-color:Brown"> Bricks</button>
+             <button style="background-color:White"> Total Cost </button>
+             <button style="background-color:White"> 80400Tk</button>
+      
+            
+         </body>
+        
+
+</html>
