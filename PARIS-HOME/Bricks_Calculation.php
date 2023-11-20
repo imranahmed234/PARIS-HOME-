1bb92df9bbd5d66e56fe4b5c5653ad7a10520176
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-11-20 18:28:55 +0600

    Add files via upload

--- PARIS-HOME/Bricks_Calculation.php
+++ PARIS-HOME/Bricks_Calculation.php
@@ -1,21 +1,40 @@
 <?php
 
-echo"PARIS-HOME<br>";
+echo"****PARIS-HOME***<br>";
 echo"Today Calculation<br>";
 echo"Number of BRICK<br>";
+echo"@14-Nov-23";
 echo"BRICK-6000<br>";
 echo"1k=13,400<br>";
-$N=6;
-$T=13400;
-
-echo "Total Cost:  $N*$T = ";
-echo $N*$T;
-echo"tk";
-
-
+$B1=6;
+$T1=13400;
+echo "Total Cost:  $B1*$T1 = ";
+echo $B1*$T1;
+echo"tk<br>";
+echo"@19-Nov-23";
+echo"BRICK-8000<br>";
+echo"1k=13,400<br>";
+$B2=8;
+$T2=13400;
+echo "Total Cost:  $B2*$T2 = ";
+echo $B2*$T2;
+echo"tk<br>";
 
 
 
 
 
 ?>
+<html>
+
+    <head>
+        <title>
+            " PARIS-HOME"
+</title>
+</head>
+        <body>
+
+</body>
+        
+
+</html>
