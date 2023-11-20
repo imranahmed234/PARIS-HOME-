27bb38c98f85fa83f2458dcec4af6ff46b1020a5
Md.Imran Ahmed <145373877+imranahmed234@users.noreply.github.com>
2023-11-20 18:34:20 +0600

    Add files via upload

--- PARIS-HOME/Bricks_cal.php
+++ PARIS-HOME/Bricks_cal.php
@@ -7,6 +7,8 @@
     </head>
          <body>
              <h1 align='center'style="background-color:Purple;"> PARIS-HOME </h1>
+             <button style="background-color:Pink"> Date</button>
+             <button style="background-color:Yellow"> 14-Nov-23</button><br>
              <button style="background-color:Brown"> Bricks</button>
              <button style="background-color:White"> 6000Tk</button></br>
              <button style="background-color:Brown"> Bricks</button>
@@ -14,7 +16,18 @@
              <button style="background-color:White"> 13400Tk</button><br>
              <button style="background-color:Brown"> Bricks</button>
              <button style="background-color:White"> Total Cost </button>
-             <button style="background-color:White"> 80400Tk</button>
+             <button style="background-color:White"> 80400Tk</button></br>
+             
+             <button style="background-color:Pink"> Date</button>
+             <button style="background-color:Yellow"> 19-Nov-23</button><br>
+             <button style="background-color:Brown"> Bricks</button>
+             <button style="background-color:White"> 8000Tk</button></br>
+             <button style="background-color:Brown"> Bricks</button>
+             <button style="background-color:White"> 1k</button>
+             <button style="background-color:White"> 13400Tk</button><br>
+             <button style="background-color:Brown"> Bricks</button>
+             <button style="background-color:White"> Total Cost </button>
+             <button style="background-color:White"> 107200Tk</button>
       
             
          </body>
